51332efbac197af1b0955bb06a7352f5d6b1f607
ashwinswaroop <ashwinswaroop93@gmail.com>
2018-07-28 21:27:17 -0400

    Created the first custom endpoint to obtain a ticker by the name of the cryptocurrency instead of the id

--- CMCKit/CMCClient.swift
+++ CMCKit/CMCClient.swift
@@ -11,7 +11,8 @@ import Foundation
 public class CMCClient {
     private let baseEndpoint = "https://api.coinmarketcap.com/v2/"
     private let session = URLSession(configuration: .default)
-    public func request<T: CMCRequest>(_ request: T, callback: @escaping (Result<T.Response>)->Void){
+    //CoinMarketCap API endpoints
+    public func request<T: CMCRequest>(_ request: T, _ callback: @escaping (Result<T.Response>)->Void){
         let endpoint = self.endpoint(request)
         let task = session.dataTask(with: URLRequest(url: endpoint)) { data, response, error in
             if let data = data {
@@ -26,6 +27,21 @@ public class CMCClient {
             }
         }
         task.resume()
+    }
+    //Custom endpoints
+    public func tickerByName(_ name: String, callback: @escaping (Result<TickerResponse>)->Void){
+        self.request(ListingsRequest()){ response in
+            switch response {
+            case .success(let data):
+                for element in data.data {
+                    if element.name == name {
+                        self.request(TickerRequest(id: element.id), callback)
+                    }
+                }
+            case .failure(let error):
+                print(error);
+            }
+        }
     }
     private func endpoint<T: CMCRequest>(_ request: T) -> URL {
         if let params = request.parameters {
